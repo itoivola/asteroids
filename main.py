f5a6a7b1ab25da1378509bfacfc12379b1d1493b
Ilkka Toivola <itoivola@gmail.com>
2025-01-11 10:00:28 +0200

    asteroids split on hit

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def main():
             for shot in shots:
                 if asteroid.collision(shot):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         screen.fill(color="black")
         for d in drawable:
